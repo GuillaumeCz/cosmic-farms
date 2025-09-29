bd51ad09ce9c7eeeec92774d9de0b326eaf335d6
115c7a5fac <115c7a5fac+github@pm.com>
2025-09-29 19:02:34 +0200

    Add antd

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,27 @@
-import './App.css'
+import { Layout } from 'antd';
+
+const { Header, Content } = Layout;
 
 function App() {
   return (
-    <>
-      Hello !
-    </>
-  )
-}
+    <Layout>
+      <Header style={{ display: 'flex', alignItems: 'center', color: 'white' }}>
+        Hello
+      </Header>
+      <Layout style={{ padding: '0 24px 24px' }}>
+        <Content
+          style={{
+            padding: 24,
+            margin: 0,
+            minHeight: 280,
+          }}
+        >
+          Content
+        </Content>
+      </Layout>
+    </Layout>
+  );
+};
 
-export default App
+export default App;
+
